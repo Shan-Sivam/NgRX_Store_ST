30e84d2745a8f46616fa2e5bb24f857c3ada318f
Shan Sivam <Shan.Sivam@drax.com>
2021-03-30 13:51:28 +0100

    added grapes

--- src/app/mocks/products.mock.ts
+++ src/app/mocks/products.mock.ts
@@ -25,4 +25,12 @@ export const productsMock: Product[] = [
     price: 1.05,
     imagePath: 'banana.jpg',
   },
+  {
+    id: 4,
+    name: 'Grapes',
+    detail:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    price: 1.05,
+    imagePath: 'grapes.jpg',
+  },
 ];
